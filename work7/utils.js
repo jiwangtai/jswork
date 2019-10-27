37cc5273e55ce56cd48dd20b03929201f00d2057
jiwangtai <1874336329@qq.com>
2019-10-27 19:03:39 +0800

    work7

--- work7/utils.js
+++ work7/utils.js
@@ -11,9 +11,9 @@ function bubbleSort(str){
 }
 function inserSort(str) {
     let arr = str.split(',')
-    for (let i =1; i<arr.length; i++){
-        for(let j = i; j > 0; j--){
-            if (arr[j - 1] > arr[j]){
+    for (let i =1; i<arr.length; i++) {
+        for(let j = i; j > 0; j--) {
+            if (arr[j - 1] > arr[j]) {
                 [arr[j - 1],arr[j]] = [arr[j],arr[j - 1]]
             }
         }
